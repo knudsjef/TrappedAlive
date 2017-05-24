7c7069be56204c15b283a5fd434212265c465c24
knudsjef <jeffery.knudson@gmail.com>
2017-05-24 16:15:52 -0500

    Updated Unity
    I updated Unity so these files changed

--- Trapped Alive Take Two/Assets/Scripts/MovingPlatform.cs
+++ Trapped Alive Take Two/Assets/Scripts/MovingPlatform.cs
@@ -25,16 +25,16 @@ public class MovingPlatform : MonoBehaviour {
         {
             if (Up)
             {
-                this.transform.FindChild("Platform").position = Vector3.MoveTowards(this.transform.FindChild("Platform").position, this.transform.FindChild("TopPoint").position, Time.deltaTime);
-                if (Vector2.Distance(this.transform.FindChild("TopPoint").position, this.transform.FindChild("Platform").position) < 0.1)
+                this.transform.Find("Platform").position = Vector3.MoveTowards(this.transform.Find("Platform").position, this.transform.Find("TopPoint").position, Time.deltaTime);
+                if (Vector2.Distance(this.transform.Find("TopPoint").position, this.transform.Find("Platform").position) < 0.1)
                 {
                     Up = false;
                 }
             }
             else
             {
-                this.transform.FindChild("Platform").position = Vector3.MoveTowards(this.transform.FindChild("Platform").position, this.transform.FindChild("BottomPoint").position, Time.deltaTime);
-                if (Vector2.Distance(this.transform.FindChild("BottomPoint").position, this.transform.FindChild("Platform").position) < 0.1)
+                this.transform.Find("Platform").position = Vector3.MoveTowards(this.transform.Find("Platform").position, this.transform.Find("BottomPoint").position, Time.deltaTime);
+                if (Vector2.Distance(this.transform.Find("BottomPoint").position, this.transform.Find("Platform").position) < 0.1)
                 {
                     Up = true;
                 }
