8f1e80b6d4c4da26e3e519c08978b68370423673
knudsjef <jeffery.knudson@gmail.com>
2017-08-09 16:28:51 -0500

    Fixed camera script and added comments to my scripts
    Commented ALL my scripts oh and I changed the camera script a bit

--- Trapped Alive Take Two/Assets/Scripts/CameraController.cs
+++ Trapped Alive Take Two/Assets/Scripts/CameraController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     [SerializeField]
+    //The player game object
     GameObject Player;
 
     [SerializeField]
+    //The speed the camera will move
     float CamSpeed;
+
+    [SerializeField]
+    //The distance from the player the camera can get
+    int CamDistance = 5;
 
 	// Use this for initialization
 	void Start ()
@@ -19,16 +25,22 @@ public class CameraController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        //If the player is moving right
         if(Player.GetComponent<Rigidbody2D>().velocity.x > 0)
         {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x + 10, this.transform.position.y, this.transform.position.z), CamSpeed * Time.deltaTime);
+            //Move the camera right by CAM DISTANCE
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x + CamDistance, this.transform.position.y, this.transform.position.z), CamSpeed * Time.deltaTime);
         }
+        //If the player is moving left
         else if(Player.GetComponent<Rigidbody2D>().velocity.x < 0)
         {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x - 10, this.transform.position.y, this.transform.position.z), CamSpeed * Time.deltaTime);
+            //Move the camera left by CAM DISTANCE
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x - CamDistance, this.transform.position.y, this.transform.position.z), CamSpeed * Time.deltaTime);
         }
+        //If the player isn't moving
         else
         {
+            //Center the camera
             this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x, this.transform.position.y, this.transform.position.z),15 * Time.deltaTime);
         }
     }
--- Trapped Alive Take Two/Assets/Scripts/Lever.cs
+++ Trapped Alive Take Two/Assets/Scripts/Lever.cs
@@ -3,19 +3,25 @@ using System.Collections;
 
 public class Lever : MonoBehaviour {
 
+    //If the lever is on or off
     public bool On;
 
+    //The position it starts at
     char StartPos;
 
-	// Use this for initialization
+	// When the game starts
 	void Start () {
 
+        //If the lever is inverted
         if (this.transform.localScale.x == -1)
         {
+            //It starts left
             StartPos = 'L';
         }
+        //If the lever is normal
         else
         {
+            //The lever starts right
             StartPos = 'R';
         }
 
@@ -26,33 +32,51 @@ public class Lever : MonoBehaviour {
 	
 	}
 
+    /******************************************************************************
+     *          The SWITCH IO function handles turning on and off the lever       *
+     ******************************************************************************/
+
     void SwitchIO()
     {
+        //Flip the lever
         this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+
+        //If the lever is on
         if (On)
         {
+            //Turn the lever off
             On = false;
         }
+        //If the lever is off
         else
         {
+            //Turn the lever on
             On = true;
         }
 
+        //If the lever is Left
         if (StartPos == 'L')
         {
+            //Turn it Right
             StartPos = 'R';
         }
+        //If the lever is Right
         else
         {
+            //Turn it Left
             StartPos = 'L';
         }
     }
 
+    //This function is called when this game object collides with anything
     void OnTriggerEnter2D(Collider2D Col)
     {
+        //If the player collides with this game object
         if (Col.transform.name == "Player")
         {
+            //Check that the player is hitting it from the correct side
             if((StartPos == 'L' && !Col.transform.GetComponent<PlayerMovement>().Left) || (StartPos == 'R' && Col.transform.GetComponent<PlayerMovement>().Left))
+                //Turn the lever on or off
             SwitchIO();
         }
     }
