6de57a81e6412fec5b29043ca20c158958b5eb9e
knudsjef <jeffery.knudson@gmail.com>
2017-08-17 20:23:22 -0500

    Got the Cam script working

--- Trapped Alive Take Two/Assets/Scripts/CameraController.cs
+++ Trapped Alive Take Two/Assets/Scripts/CameraController.cs
@@ -2,32 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     [SerializeField]
+    //The player game object
     GameObject Player;
-    [SerializeField]
-    float Damp;
-    float x;
 
-	// Use this for initialization
-	void Start ()
+    [SerializeField]
+    //The speed the camera will move
+    float CamSpeed;
+
+    [SerializeField]
+    //The distance from the player the camera can get
+    int CamDistance = 5;
+
+    // Use this for initialization
+    void Start()
     {
-        x = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If the player is moving right
         if (Player.GetComponent<Rigidbody2D>().velocity.x > 0)
         {
-            x += Time.deltaTime * Damp;
-            this.transform.position = new Vector3(x, this.transform.position.y, -10);
+            //Move the camera right by CAM DISTANCE
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x + CamDistance, this.transform.position.y, this.transform.position.z), CamSpeed * Time.deltaTime);
         }
-        else if(Player.GetComponent<Rigidbody2D>().velocity.x < 0)
+        //If the player is moving left
+        else if (Player.GetComponent<Rigidbody2D>().velocity.x < 0)
         {
-            x -= Time.deltaTime * Damp;
-            this.transform.position = new Vector3(x, this.transform.position.y, -10);
+            //Move the camera left by CAM DISTANCE
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x - CamDistance, this.transform.position.y, this.transform.position.z), CamSpeed * Time.deltaTime);
+        }
+        //If the player isn't moving
+        else
+        {
+            //Center the camera
+            this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(Player.transform.position.x, this.transform.position.y, this.transform.position.z), 15 * Time.deltaTime);
         }
     }
 }
