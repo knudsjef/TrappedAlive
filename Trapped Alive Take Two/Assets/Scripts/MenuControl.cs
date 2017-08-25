a453ac567cb66f4ccd73e4646f80eb1296750422
knudsjef <jeffery.knudson@gmail.com>
2017-08-25 10:48:02 -0500

    Created resolution and fullscreen toggles

--- Trapped Alive Take Two/Assets/Scripts/MenuControl.cs
+++ Trapped Alive Take Two/Assets/Scripts/MenuControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuControl : MonoBehaviour {
 
@@ -20,9 +21,23 @@ public class MenuControl : MonoBehaviour {
     [SerializeField]
     GameObject ControlMenu;
 
+    [SerializeField]
+    Dropdown ResDrop;
+
+    bool Full;
+
     void Start()
     {
         BackToMenu();
+
+        if(Screen.fullScreen == true)
+        {
+            PlayerPrefs.SetInt("Fullscreen", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Fullscreen", 0);
+        }
     }
     
     public void NewGame()
@@ -64,6 +79,9 @@ public class MenuControl : MonoBehaviour {
     public void BackToMenu()
     {
         SettingsMenu.SetActive(false);
+        AudioSettingsMenu.SetActive(false);
+        VideoSettingsMenu.SetActive(false);
+        ControlMenu.SetActive(false);
         MainMenu.SetActive(true);
     }
 
@@ -73,6 +91,161 @@ public class MenuControl : MonoBehaviour {
         VideoSettingsMenu.SetActive(false);
         ControlMenu.SetActive(false);
         SettingsMenu.SetActive(true);
+    }
+
+    public void ChangeResolution()
+    {
+        switch (ResDrop.value)
+        {
+            case 0:
+                PlayerPrefs.SetInt("ScreenWidth", 1600);
+                PlayerPrefs.SetInt("ScreenHeight", 675);
+                break;
+
+            case 1:
+                PlayerPrefs.SetInt("ScreenWidth", 1920);
+                PlayerPrefs.SetInt("ScreenHeight", 810);
+                break;
+
+            case 2:
+                PlayerPrefs.SetInt("ScreenWidth", 1920);
+                PlayerPrefs.SetInt("ScreenHeight", 1080);
+                break;
+
+            case 3:
+                PlayerPrefs.SetInt("ScreenWidth", 1680);
+                PlayerPrefs.SetInt("ScreenHeight", 1050);
+                break;
+
+            case 4:
+                PlayerPrefs.SetInt("ScreenWidth", 1600);
+                PlayerPrefs.SetInt("ScreenHeight", 900);
+                break;
+
+            case 5:
+                PlayerPrefs.SetInt("ScreenWidth", 1440);
+                PlayerPrefs.SetInt("ScreenHeight", 900);
+                break;
+
+            case 6:
+                PlayerPrefs.SetInt("ScreenWidth", 1400);
+                PlayerPrefs.SetInt("ScreenHeight", 1050);
+                break;
+
+            case 7:
+                PlayerPrefs.SetInt("ScreenWidth", 1366);
+                PlayerPrefs.SetInt("ScreenHeight", 768);
+                break;
+
+            case 8:
+                PlayerPrefs.SetInt("ScreenWidth", 1360);
+                PlayerPrefs.SetInt("ScreenHeight", 768);
+                break;
+
+            case 9:
+                PlayerPrefs.SetInt("ScreenWidth", 1280);
+                PlayerPrefs.SetInt("ScreenHeight", 1024);
+                break;
+
+            case 10:
+                PlayerPrefs.SetInt("ScreenWidth", 1280);
+                PlayerPrefs.SetInt("ScreenHeight", 960);
+                break;
+
+            case 11:
+                PlayerPrefs.SetInt("ScreenWidth", 1280);
+                PlayerPrefs.SetInt("ScreenHeight", 768);
+                break;
+
+            case 12:
+                PlayerPrefs.SetInt("ScreenWidth", 1280);
+                PlayerPrefs.SetInt("ScreenHeight", 800);
+                break;
+
+            case 13:
+                PlayerPrefs.SetInt("ScreenWidth", 1280);
+                PlayerPrefs.SetInt("ScreenHeight", 720);
+                break;
+
+            case 14:
+                PlayerPrefs.SetInt("ScreenWidth", 1152);
+                PlayerPrefs.SetInt("ScreenHeight", 864);
+                break;
+
+            case 15:
+                PlayerPrefs.SetInt("ScreenWidth", 1024);
+                PlayerPrefs.SetInt("ScreenHeight", 768);
+                break;
+
+            case 16:
+                PlayerPrefs.SetInt("ScreenWidth", 1280);
+                PlayerPrefs.SetInt("ScreenHeight", 540);
+                break;
+
+            case 17:
+                PlayerPrefs.SetInt("ScreenWidth", 800);
+                PlayerPrefs.SetInt("ScreenHeight", 600);
+                break;
+
+            case 18:
+                PlayerPrefs.SetInt("ScreenWidth", 640);
+                PlayerPrefs.SetInt("ScreenHeight", 400);
+                break;
+
+            case 19:
+                PlayerPrefs.SetInt("ScreenWidth", 512);
+                PlayerPrefs.SetInt("ScreenHeight", 384);
+                break;
+
+            case 20:
+                PlayerPrefs.SetInt("ScreenWidth", 640);
+                PlayerPrefs.SetInt("ScreenHeight", 480);
+                break;
+
+            case 21:
+                PlayerPrefs.SetInt("ScreenWidth", 400);
+                PlayerPrefs.SetInt("ScreenHeight", 300);
+                break;
+
+            case 22:
+                PlayerPrefs.SetInt("ScreenWidth", 320);
+                PlayerPrefs.SetInt("ScreenHeight", 200);
+                break;
+
+            case 23:
+                PlayerPrefs.SetInt("ScreenWidth", 320);
+                PlayerPrefs.SetInt("ScreenHeight", 240);
+                break;
+            
+            default:
+                PlayerPrefs.SetInt("ScreenWidth", 1920);
+                PlayerPrefs.SetInt("ScreenHeight", 1080);
+                break;
+        }
+        if(PlayerPrefs.GetInt("Fullscreen") == 0)
+        {
+            Full = false;
+        }
+        else
+        {
+            Full = true;
+        }
+
+        Screen.SetResolution(PlayerPrefs.GetInt("ScreenWidth"), PlayerPrefs.GetInt("ScreenHeight"), Full);
+
+    }
+
+    public void FullscreenIO()
+    {
+        if(PlayerPrefs.GetInt("Fullscreen") == 0)
+        {
+            PlayerPrefs.SetInt("Fullscreen", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Fullscreen", 0);
+        }
+        ChangeResolution();
     }
 
     public void Quit()
